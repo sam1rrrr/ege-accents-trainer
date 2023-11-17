27cfeaf57ea91e67dcb4faa25ac7900ae9428f67
Samir <54107548+sam1rrrr@users.noreply.github.com>
2023-11-17 21:37:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ app = Flask(__name__)
 file = open('words.json', encoding='utf8')
 words = json.load(file)
 words_keys = list(words.keys())
-print(words_keys)
-print()
+
 @app.route('/')
 def main_page():
     word = random.choice(words_keys)
